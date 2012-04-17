3c2959c898959abc70005c3c7783b7b00f7c4f22
tochi <tochi@aguuu.com>
2012-04-17 16:57:43 +0900

    Initial Commit

---
+++ ScrollSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ScrollSample
+//
+//  Created by 智行 栩平 on 12/04/17.
+//  Copyright (c) 2012年 aguuu Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ScrollSample/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  ScrollSample
+//
+//  Created by 智行 栩平 on 12/04/17.
+//  Copyright (c) 2012年 aguuu Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+  return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
